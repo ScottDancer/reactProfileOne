9e6bbbdb01b9c0b6e5699229d33cfb9c4241c2f1
Scott Dancer <scottpdancer@gmail.com>
2020-11-11 09:27:01 -0800

    profile page layout complete

---
+++ src/pages/Home/index.js
@@ -0,0 +1,82 @@
+import React from "react";
+import "./style.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1 style={{ color: "grey" }}>
+        Hello, I’m Scott Dancer and I’ve gone digital!
+      </h1>
+      <img
+        src="https://res-console.cloudinary.com/dpnjzg0ar/thumbnails/transform/v1/image/upload//v1605085066/cmVkX2dsb3dfejJ3Y3g3/drilldown"
+        alt="red glow face pic"
+      />
+      <img
+        src="https://res-console.cloudinary.com/dpnjzg0ar/thumbnails/transform/v1/image/upload//v1605085301/b3JhbmdlX2dsb3dfcmQ0YnJi/drilldown"
+        alt="orange glow face pic"
+      />
+      <img
+        src="https://res-console.cloudinary.com/dpnjzg0ar/thumbnails/transform/v1/image/upload//v1605085489/eWVsbG93X2dsb3dfd2Z6eXR4/drilldown"
+        alt="yellow glow face pic"
+      />
+      <img
+        src="https://res-console.cloudinary.com/dpnjzg0ar/thumbnails/transform/v1/image/upload//v1605085870/bmVvbl9ncmVlbl9nbG93X3NvcnhuaA==/drilldown"
+        alt="neon green glow face pic"
+      />
+      <img
+        src="https://res-console.cloudinary.com/dpnjzg0ar/thumbnails/transform/v1/image/upload//v1605085988/Ymx1ZV9nbG93X2Vpajc4Ng==/drilldown"
+        alt="blue glow face pic"
+      />
+      <img
+        src="https://res-console.cloudinary.com/dpnjzg0ar/thumbnails/transform/v1/image/upload//v1605086226/cHVycGxlX2dsb3dfazFvMGxz/drilldown"
+        alt="purple glow face pic"
+      />
+
+      <p>
+        I have graciously worked in the restaurant industry for over two and a
+        half decades. Mostly in Boston and mainly behind the bars at fine dining
+        establishments.
+      </p>
+      <p>
+        I now live in the Pacific Northwest where I am taking my artistic
+        passions and bringing them to the digital world. I am a full stack web
+        developer with a background in fine arts. I studied writing and music
+        theory at Harvard Extension School and learned color theory and life
+        drawing at the Museum of Fine Arts in Boston, MA. I received two
+        certificates in San Diego, CA for Visual, Vector, and Digital Design and
+        Multi-Media Design.
+      </p>
+      <p>
+        {" "}
+        My most recent achievement is a Full Stack Web Developing Certificate
+        from the University of Washington’s Coding Bootcamp certified through
+        Trilogy Education Services.
+      </p>
+      <p>
+        {" "}
+        I have always been creative, and I have had a long desire of learning to
+        build websites completely from the ground up. I am finding great joy in
+        this challenging endeavor and look forward to future projects.
+      </p>
+      <p>
+        {" "}
+        Quirky, cartoonish, and a bit nerdy would describe who I am and that
+        certainly is reflected in my design style. Fluent in front end and back
+        end languages including many of the apps with the Adobe Creative Suite
+        including: Photo Shop, InDesign, Lightroom, Illustrator, Xd, HTML. CSS,
+        JavaScript, jQuery, Node.js, Heroku, Git, Responsive Design, User
+        Authentication, Materialize, Bootstrap, MySQL, MongoDB and continue to
+        learn more each day.
+      </p>
+      <p>
+        I could not be more excited about finding opportunities within the tech
+        industry to build a satisfying career. My current endeavors consist of
+        two websites built entirely by me to showcase my unique styles, digital
+        and hand drawn images along with my technical skills. Black Diamond
+        Graphics will display multiple levels of design with a great emphasis on
+        winter sports and Graphic Graphic Design will be dedicated to present
+        material intended for the mature audience.{" "}
+      </p>
+    </div>
+  );
+}
