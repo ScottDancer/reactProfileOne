53e52abe97e76c130b1a94ad4c3e032e5a10bc99
Scott Dancer <scottpdancer@gmail.com>
2020-11-23 00:34:07 -0800

    added contact page

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,10 @@
+function Contact() {
+  return (
+      <div>
+          <h1>This is the contact page.</h1>
+          Please feel free to contact me directly at my email: 
+          scottpdancer@gmail.com
+
+      </div>
+  )
+}
