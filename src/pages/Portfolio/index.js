62748a046895e51b1ead0d7cd2f015c19c02d570
Scott Dancer <scottpdancer@gmail.com>
2020-11-24 04:10:48 -0800

    arranged photos

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -22,20 +22,20 @@ function Portfolio() {
         <div style = {{textAlign: "center"}}>
          
             <h1>Group Projects</h1>
-            <h4>Click On the Images To View The Websites</h4>
+            <h3>Click On the Images To View The Websites</h3>
             <ul>
                 <li>
-                    <Link to={{ pathname: "https://gnuartemis.github.io/Music-Chord-Identifier/index.html" }} target="_blank"><img className ="profile-image" src = {Project1a}></img><img className ="profile-image" src = {Project1b}></img></Link>
+                    <Link to={{ pathname: "https://gnuartemis.github.io/Music-Chord-Identifier/index.html" }} target="_blank"><img className ="profile-image" src = {Project1b}></img><img className ="profile-image" src = {Project1a}></img></Link>
                 </li>
-                <h3>Achording to Jazz: Music Theory app for 5th grade music students</h3>
+                <h2>Achording to Jazz: Music Theory app for 5th grade music students</h2>
                 <li>
-                    <Link to={{ pathname: "https://the-cold-west.herokuapp.com/" }} target="_blank"><img className ="profile-image" src = {Project2a}></img><img className ="profile-image" src = {Project2b}></img></Link>
+                    <Link to={{ pathname: "https://the-cold-west.herokuapp.com/" }} target="_blank"><img className ="profile-image" src = {Project2b}></img><img className ="profile-image" src = {Project2a}></img></Link>
                 </li>
-                <h3>The Cold West: A winter adventure touring website</h3>
+                <h2>The Cold West: A winter adventure touring website</h2>
                 <li>
                     <Link to={{ pathname: "https://experience-indulgence.herokuapp.com/" }} target="_blank"><img className ="profile-image" src = {Project3a}></img><img className ="profile-image" src = {Project3b}></img></Link>
                 </li>
-                <h3>Indulge: An upscale private chef dining service</h3>
+                <h2>Indulge: An upscale private chef dining service</h2>
             </ul>
         </div>
     )
