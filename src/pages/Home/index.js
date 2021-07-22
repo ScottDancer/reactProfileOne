eba0469d5fc535e5f96f756c628cb837baf507b3
Scott Dancer <scottpdancer@gmail.com>
2021-07-22 10:18:32 -0700

    connected hero, imported fonts, updated text on home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import "./style.css";
+import "./home-styles.css";
 import { Link } from "react-router-dom";
 import FacePic from "./assets/headshot.jpg"
+import hero from "./assets/hero-comp.jpg"
 
 export default function Home() {
   return (
@@ -11,61 +12,29 @@ export default function Home() {
     //   <Link to="/Contact"> Contact </Link>
     // </div>
     <div style = {{textAlign: "center"}} className="Home">
-      <h1 style={{ color: "#003997"}}>
-        Hello, I’m Scott Dancer and I’ve gone digital!
+      <h1 style={{ color: "#877D74"}}>
+        Scott Patrcick Dancer
       </h1>
- 
-      <img src = {FacePic} className="profilePic" alt = "headshot"></img>
+      <img src = {hero} className="hero" alt ="glowing laptop"></img>
+      <img src = {FacePic} className="profilePic" alt ="headshot"></img>
   
-      {/* <img
-        src="https://res-console.cloudinary.com/dpnjzg0ar/thumbnails/transform/v1/image/upload//v1606120852/Ymx1ZV9nbG93X3FvcGduaA==/drilldown"
-        alt="blue glow face pic"
-      /> */}
       <div className="container">
-      <h4>
-        I have graciously worked in the restaurant industry for over two and a
-        half decades. Mostly in Boston and mainly tending bars at fine dining
-        establishments.
-      </h4>
-      <h4>
-        I now live in the Pacific Northwest where I am taking my artistic
-        passions and bringing them to the digital world. I am a full stack web
-        developer with a background in fine arts. I studied writing and music
-        theory at Harvard Extension School and learned color theory and life
-        drawing at the Museum of Fine Arts in Boston, MA. I received two
-        certificates in San Diego, CA for Visual, Vector, Digital and
-        Multi-Media Design.
-      </h4>
-      <h4>
-        {" "}
-        My most recent achievement is a Full Stack Web Developing Certificate
-        from the University of Washington’s Coding Bootcamp certified through
-        Trilogy Education Services.
-      </h4>
-      <h4>
-        {" "}
-        I have always been creative, and I have had a long desire of learning to
-        build websites completely from the ground up. I am finding great joy in
-        this challenging endeavor and look forward to future projects.
-      </h4>
-      <h4>
-        {" "}
-        Quirky, cartoonish, and a bit nerdy would describe who I am and that
-        certainly is reflected in my design style. Fluent in front end and back
-        end languages including many of the apps with the Adobe Creative Suite
-        including: Photo Shop, InDesign, Lightroom, Illustrator, Xd, HTML. CSS,
-        JavaScript, jQuery, Node.js, Heroku, Git, Responsive Design, User
-        Authentication, Materialize, Bootstrap, MySQL, MongoDB and continue to
-        learn more each day.
-      </h4>
-      <h4>
-        I could not be more excited about finding opportunities within the tech
-        industry to build a satisfying career. My current endeavors consist of
-        two websites built entirely by me to showcase my unique styles, digital
-        and hand drawn images along with my technical skills. Black Diamond
-        Graphix will display multiple levels of design with a great emphasis on
-        winter sports and Graphic Graphic Design will be edgier andm dedicated for adult humor.{" "}
-      </h4>
+        <p>Full Stack Web Developer and Graphic Designer with a background in
+        illustration and cartooning. Possess a strong foundation in
+        communication, problem solving, and creativity that lends itself to
+        building applications that are aesthetically pleasing and meet
+        client expectations. Earned a Certificate in Web Development from
+        the University of Washington.</p>
+
+        <p>
+          Vancouver, WA | (360) 953-7969 
+          Email:        scottpdancer@gmail.com
+          GitHub:      https://github.com/ScottDancer
+          Portfolio:   https://spd-react-profile.herokuapp.com/home
+          LinkedIn:   https://www.linkedin.com/in/scott-dancer-9744091b2/
+      </p>
+    
+  
       </div>
     </div>
   );
